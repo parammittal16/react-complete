b21ff3c0f6903cb4749ff5561e6797a88e7ce2f7
rishabhscross <parammittal16@gmail.com>
2018-11-21 18:46:03 +0530

    Add firebase connection

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   render () {
     return (
-      <div>
+      <BrowserRouter>
         <Layout>
-          <BurgerBuilder />
+          <Switch>
+          <Route path="/checkout" component={Checkout}/>
+          <Route path="/" exact component={BurgerBuilder}/>
+          </Switch>
         </Layout>
-      </div>
+      </BrowserRouter>
     );
   }
 }
