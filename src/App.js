d02452cd64b4ff01de12adfa127b475ec4146cbc
rishabhscross <parammittal16@gmail.com>
2018-11-21 22:10:13 +0530

    Add routing and order placing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   render () {
     return (
-      <BrowserRouter>
+      <div>
         <Layout>
           <Switch>
-          <Route path="/checkout" component={Checkout}/>
-          <Route path="/" exact component={BurgerBuilder}/>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
-      </BrowserRouter>
+      </div>
     );
   }
 }
