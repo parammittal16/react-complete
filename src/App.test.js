0b34262d21d3a97f2122234500e0f0fe6de8c39b
rishabhscross <parammittal16@gmail.com>
2018-11-24 14:31:41 +0530

    Correct index test js

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
+
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    order: orderReducer,
+    auth: authReducer
+});
+
 
 it("renders without crashing", () => {
+  const store = createStore(rootReducer, applyMiddleware(thunk));
   const div = document.createElement("div");
   ReactDOM.render(
+    <Provider store={store}>
 <BrowserRouter>
   <App />
-</BrowserRouter>,
+</BrowserRouter>
+</Provider>,
 div
   );
   ReactDOM.unmountComponentAtNode(div);
