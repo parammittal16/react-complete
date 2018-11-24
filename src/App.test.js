159c100fef04a7ae20a1e672b4a196125e867074
rishabhscross <parammittal16@gmail.com>
2018-11-24 13:52:39 +0530

    Update index test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    , div);
-  });
-  
+<BrowserRouter>
+  <App />
+</BrowserRouter>,
+div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+})
